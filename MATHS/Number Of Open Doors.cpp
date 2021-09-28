23bba846c0cf4cdd363954ef1b2df8c0e4d6de70
Shreyansh Tiwari <60467135+coziamshreyansh@users.noreply.github.com>
2021-09-28 15:31:26 +0530

    Create Number Of Open Doors.cpp

---
+++ MATHS/Number Of Open Doors.cpp
@@ -0,0 +1,8 @@
+public:
+    int noOfOpenDoors(long long N) {
+        // code here
+        int opendoors = sqrt(N);
+        // no. of perfect sq.s bw 1 to N is sqrt(N)
+        return opendoors;
+        
+    }
